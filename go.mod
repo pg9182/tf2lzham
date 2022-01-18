1878b0dbd38dbd290115a6218340ac012aeddf8a
pg9182 <96569817+pg9182@users.noreply.github.com>
2022-01-18 15:36:49 -0500

    Initialize Go module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pg9182/lzham
+
+go 1.16
---
+++ lzham.go
@@ -0,0 +1,6 @@
+// Package lzham wraps a subset of the LZHAM library.
+package lzham
+
+// #cgo CFLAGS: -DLZHAM_ANSI_CPLUSPLUS
+// #cgo CXXFLAGS: -DLZHAM_ANSI_CPLUSPLUS
+import "C"
